e6e4f581f09ee664a4d4d6d9c7bd41aabf51d106
Vanessa Madu <vm2218@ic.ac.uk>
2024-04-22 15:11:16 +0100

    partial series

--- numerical_M.py
+++ numerical_M.py
@@ -5,11 +5,26 @@ def uniform_char_func(n,L,S,phi):
     description:    finite approximation of the characteristic function of an infinite geometrically weighted
                     sum of uniformly distributed random variables on the interval [-1,1].
     params:     
-    n:              integer
-    L:              real number
-    S:              integer (upper value for infinite product estimation)
-    phi:            parameter [0,1)     
+    n:              integer > 0
+    L:              real number > 0
+    S:              integer > 0 (upper value for infinite product estimation)
+    phi:            real number [0,1)     
 
-    returns: Approximation of infinite product of sinc(n phi^s/L) wrt s     
+    returns:        approximation of infinite product of sinc(n phi^s/L) wrt s     
     '''
     return np.prod([np.sinc((phi**s)*n/L) for s in range(S)])
+
+def partial_alternating_sum(k,L,S,phi,f):
+    '''
+    description:    finite approximation of the alternating series sum(char_func(n))
+
+    params:
+    k:              integer > 0 (number of terms included in the series)
+    L:              real number > 0
+    S:              integer > 0 (upper value for infinite product estimation)
+    phi:            real number: [0,1)
+    f:              characteristic function
+
+    returns:        approximation of the alternating series for n = 1,...,k+1
+    '''
+    return np.sum([(-1)**(n-1) *f(n,L,S,phi) for n in range(1,k+1)])
