ec784657f28283be5a90463e89fe22cf95c5e153
Vanessa Madu <vm2218@ic.ac.uk>
2024-05-21 13:52:33 +0100

    even more done!

--- beta.py
+++ beta.py
@@ -1,15 +1,21 @@
 from numerical_M import *
 import matplotlib.pyplot as plt
 
-phi = 0.5
+# params
+alpha = 2
+# Specifying L
+phi = 0.8
+L = geom_L(phi,0.5)
 S = 200 
-tol = 10E-4
-p = 20 
-alpha = 0.25
+coeff = geom_coeff
+coeff_param = phi
+params = [alpha, S, coeff,coeff_param,L]
+# estimator parameters
+tol = 10E-6
+p = 10
 
-params = [geom_L(phi),S,phi,alpha]
-
-vals = approx_alternating_series(params,tol,p,beta_char_func)
-print(np.real(vals[3]))
-plt.plot([np.real(x) for x in vals[2]])
+print(M(params,tol,p,uniform_char_func))
+vals = approx_alternating_series(params,tol,p,uniform_char_func)
+print(vals[3])
+plt.plot(vals[2])
 plt.show()
