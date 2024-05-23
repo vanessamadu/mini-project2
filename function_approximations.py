895fd18e2e2a2f5833e0761177bf9c7e974478b9
Vanessa Madu <vm2218@ic.ac.uk>
2024-05-23 12:29:12 +0100

    single uniform

--- function_approximations.py
+++ function_approximations.py
@@ -13,3 +13,14 @@ def beta_char_func(t,beta_params):
     alpha, B = beta_params
     return np.real_if_close((2*B)**(2*alpha-1)*np.exp(1j*B*t)*hyp1f1(alpha,2*alpha,2j*B*t))
 
+def uniform_char_func(t,uniform_params):
+    '''
+    description:    characteristic function of a uniform distributed random variable
+                    on the interval [-B,B]
+    params:
+    t:              real number > 0
+    uniform_params: B, real number > 0
+    '''
+    # alphabetically unpack
+    B = uniform_params
+    return np.sinc(B*t/np.pi)
