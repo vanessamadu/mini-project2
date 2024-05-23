1fd3f3069f7f60689b753304bc75461eb12ff7ca
Vanessa Madu <vm2218@ic.ac.uk>
2024-05-23 12:24:56 +0100

    single beta

---
+++ function_approximations.py
@@ -0,0 +1,15 @@
+import numpy as np
+from scipy.special import hyp1f1
+
+def beta_char_func(t,beta_params):
+    '''
+    description:    characteristic function of a symmetric beta distributed random
+                    variable on the interval [-B,B] with parameter alpha.
+    params:
+    t:              real number > 0
+    beta_params:    [B,alpha], real numbers > 0       
+    '''
+    # alphabetically unpack
+    alpha, B = beta_params
+    return np.real_if_close((2*B)**(2*alpha-1)*np.exp(1j*B*t)*hyp1f1(alpha,2*alpha,2j*B*t))
+
