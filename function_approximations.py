3d8f8e07d6c3e21fb12eb9e03b146e903067529a
Vanessa Madu <vm2218@ic.ac.uk>
2024-05-24 22:12:14 +0100

    minor corrections

--- function_approximations.py
+++ function_approximations.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.special import hyp1f1
-from abc import ABC,abstractmethod
+import scipy.integrate as integrate
 
 #--------------- STANDARD SINGLE RV CHARACTERISTIC FUNCTIONS --------------#
 def beta_char_func(t,beta_params):
@@ -14,7 +14,7 @@ def beta_char_func(t,beta_params):
     # unpack
     alpha, B = beta_params
 
-    return np.real_if_close((2*B)**(2*alpha-1)*np.exp(1j*B*t)*hyp1f1(alpha,2*alpha,2j*B*t))
+    return np.real_if_close((2*B)**(2*alpha-1)*np.exp(-1j*B*t)*hyp1f1(alpha,2*alpha,2j*B*t))
 
 def uniform_char_func(t,uniform_params):
     '''
@@ -76,6 +76,7 @@ def partial_alternating_sum(L_func,L_params,S,c_s_func,c_s_params,cf_params,cf,k
     return np.sum([(-1)**(n-1)*infinite_weighted_sum_RV_char_func(n*np.pi/L,S,c_s_func,c_s_params,cf_params,cf) 
                    for n in range(1,k+1)])
 
+
 def partial_sum_remainder(L_func,L_params,S,c_s_func,c_s_params,cf_params,cf,k):
 
     return partial_alternating_sum(L_func,L_params,S,c_s_func,c_s_params,
@@ -101,8 +102,8 @@ def approx_alternating_series(L_func,L_params,S,c_s_func,c_s_params,cf_params,cf
     return k, remainders, partial_sums, partial_alternating_sum(L_func,L_params,S,c_s_func,c_s_params,cf_params,cf,k)
 
 def M(L_func,L_params,S,c_s_func,c_s_params,cf_params,cf,tol,p):
-    vals = approx_alternating_series(L_func,L_params,S,c_s_func,c_s_params,cf_params,cf,tol,p)
-    return np.pi/(2*vals[3])
+    vals =approx_alternating_series(L_func,L_params,S,c_s_func,c_s_params,cf_params,cf,tol,p)
+    return np.real_if_close(np.pi/(2*vals[3]))
 
 #----------------- Probability Density Function -----------------#
 
@@ -113,3 +114,8 @@ def PDF(x,M_val,N,L_func,L_params,S,c_s_func,c_s_params,cf_params,cf):
     pdf*= M_val/(np.pi*L)
     pdf += 1/(2*L)
     return pdf
+
+def check_integral(M_val,N,L_func,L_params,S,c_s_func,c_s_params,cf_params,cf):
+    L = L_func(L_params)
+    result = integrate.quad(lambda x: PDF(x,M_val,N,L_func,L_params,S,c_s_func,c_s_params,cf_params,cf),-L,L)
+    return result
